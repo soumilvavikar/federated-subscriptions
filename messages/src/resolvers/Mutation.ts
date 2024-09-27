b15d225628657fdbd4fdfe1879a51464807870af
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-09-27 15:08:53 -0600

    keep commented out

--- messages/src/resolvers/Mutation.ts
+++ messages/src/resolvers/Mutation.ts
@@ -5,30 +5,30 @@ export const Mutation: Resolvers = {
     createConversation: async (_, { recipientId }, { dataSources, userId }) => {
       return dataSources.db.createNewConversation({ userId, recipientId })
     },
-    sendMessage: async (_, { message }, { dataSources, userId }) => {
-      const { conversationId, text } = message;
-      const {
-        id,
-        text: messageText,
-        sentFrom,
-        sentTo,
-        sentTime,
-        ...messageAttributes
-      } = await dataSources.db.sendMessageToConversation({
-        conversationId,
-        text,
-        userId,
-      });
+    // sendMessage: async (_, { message }, { dataSources, userId }) => {
+    //   const { conversationId, text } = message;
+    //   const {
+    //     id,
+    //     text: messageText,
+    //     sentFrom,
+    //     sentTo,
+    //     sentTime,
+    //     ...messageAttributes
+    //   } = await dataSources.db.sendMessageToConversation({
+    //     conversationId,
+    //     text,
+    //     userId,
+    //   });
     
-      // Return all of the message that was created
-      return {
-        id,
-        text: messageText,
-        sentFrom,
-        sentTo,
-        sentTime,
-        ...messageAttributes,
-      };
-    }
+    //   // Return all of the message that was created
+    //   return {
+    //     id,
+    //     text: messageText,
+    //     sentFrom,
+    //     sentTo,
+    //     sentTime,
+    //     ...messageAttributes,
+    //   };
+    // }
   }
 }
