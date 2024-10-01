8f2b456128b636b1def1e370573ec8c6b677f082
Liz Hennessy <lizhennessy@Lizs-MacBook-Pro.local>
2024-10-01 11:12:57 -0600

    remove comments

--- messages/src/resolvers/Query.ts
+++ messages/src/resolvers/Query.ts
@@ -4,13 +4,9 @@ export const Query: Resolvers = {
   Query: {
     conversations: async (_, __, { dataSources, userId }) => {
       return dataSources.db.findUserConversations(userId);
-      
-      // return dataSources.db.findUserConversations(userId);
     },
     conversation: async (_, { recipientId }, { dataSources, userId }) => {
       return dataSources.db.findUserConversationWithRecipient({ recipientId, userId });
-      
-      //return dataSources.db.findUserConversationWithRecipient({ recipientId, userId });
     }
   }
 }
